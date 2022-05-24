33ca33d7e66af4dab51b8d3e4549dbdc01abac44
kosu <kosumon27@gmail.com>
2022-05-24 17:20:23 +0900

    ✨ footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,12 +1,35 @@
+import Link from "next/link";
 import type { FC } from "react";
+
+const ITEMS = [
+  { href: "/about", label: "About" },
+  { href: "/support/terms", label: "利用規約" },
+  { href: "/support/privacy-policy", label: "プライバシーポリシー" },
+];
 
 /**
  * @package
  */
 export const Footer: FC = () => {
   return (
-    <div>
-      <small>&copy; 20xx example</small>
-    </div>
+    <footer className="py-10 px-6 mx-auto min-w-full text-center text-gray-500">
+      <ul className="inline-grid gap-3 p-0 xs:flex xs:gap-4 xs:justify-center">
+        {ITEMS.map((item) => {
+          return (
+            <li key={item.href} className="contents ">
+              <Link href={item.label}>
+                <a className="p-1 text-sm text-inherit no-underline hover:underline">
+                  {item.label}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <small className="block mt-4" lang="en">
+        &copy; 20xx example
+      </small>
+    </footer>
   );
 };
